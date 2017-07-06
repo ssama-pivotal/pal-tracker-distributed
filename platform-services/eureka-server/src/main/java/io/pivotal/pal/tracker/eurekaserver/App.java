81f17f46e26add0f69ccb3532577f90d7cf555fc
Tyson Gern <tgern@pivotal.io>
2017-07-06 16:57:30 -0600

    Enable service discovery

--- platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
+++ platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
@@ -1,6 +1,5 @@
 package io.pivotal.pal.tracker.eurekaserver;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
