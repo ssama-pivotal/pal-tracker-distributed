f6344d9a42774fd6e21781e02d6e07944ac51810
Tyson Gern <tgern@pivotal.io>
2017-07-07 16:24:52 -0600

    Add security

--- platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/App.java
+++ platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/App.java
@@ -1,6 +1,5 @@
 package io.pivotal.pal.tracker.oauthserver;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
